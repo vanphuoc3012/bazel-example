c23af3a876ec17cfa44b6f4256f21414f79209e9
vanphuoc <vanphuoc3012@gmail.com>
2023-10-15 10:11:41 +0700

    fc

---
+++ projects/calculator/calculator_test.py
@@ -0,0 +1,10 @@
+import unittest
+from projects.calculator.calculator import Calculator
+
+class Test(unittest.TestCase):
+    def test_sum(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(1,2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
